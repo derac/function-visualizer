b5b42b915823f9885f2ed49129e1d60004f0f13a
derac <dwolson123@gmail.com>
2025-08-08 13:50:25 -0500

    I actually have no idea what this code is supposed to be doing, but I don't care

--- core/nd.py
+++ core/nd.py
@@ -1,13 +1,24 @@
 """NumPy/CuPy interop helpers and array module selection."""
-from typing import Any
+from typing import TYPE_CHECKING, Union
 from utils.hardware import get_array_module
 import numpy as base_np
 
 # Select array module (numpy or cupy)
 xp = get_array_module()
 
-# Generic array type that may be a NumPy or CuPy ndarray
-Array = Any
+if TYPE_CHECKING:
+    import cupy as cp  # type: ignore
+    CuPyNDArray = cp.ndarray  # type: ignore[attr-defined]
+else:
+    try:
+        import cupy as cp  # type: ignore
+        CuPyNDArray = cp.ndarray  # type: ignore[attr-defined]
+    except Exception:
+        class CuPyNDArray:  # type: ignore
+            pass
+
+# Union of NumPy and CuPy ndarrays
+Array = Union[base_np.ndarray, "CuPyNDArray"]
 
 def to_cpu(array: Array) -> base_np.ndarray:
     """Return a NumPy array for robust CPU-side ops like percentiles.
